2cd9ad20bd84ffe6af95a1a457ff2ba256da6969
Alobaid <s202253180@ccm.kfupm.edu.sa>
2025-11-12 08:36:58 +0300

    task 1

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -107,20 +107,10 @@ LAB SETUP INSTRUCTIONS
 import express from "express";
 const app = express();
 
-
-// create server
-
-
-// Query params: /echo?name=Ali&age=22
-
-
-// Route params: /profile/First/Last
-
-
-// Route param middleware example: /users/42
-
-
-// Route params: /users/:userId route
-
-
+// ============================================
+// TODO-1: Server setup
+// ============================================
+app.listen(3000, () => {
+  console.log("API running at http://localhost:3000");
+});
 
